4507fb3d3a6ea8e4484016639975c28c34b11019
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-11 20:07:21 +0300

    new folder Task 24,27

---
+++ Task24/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+Console.Clear();
+
+/*Console.Write("Введите А: ");
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine(Summ(A));
+Console.WriteLine(Summ(int.Parse(Console.ReadLine())));
+
+int Summ(int number)
+{
+    int sum=0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
+*/
+/*Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр введенного числа : {N(num)}");
+
+int N (int num)
+{
+    int i = 0;
+    while (num>0)
+    {
+      num/=10;
+      i++;
+    }
+    return i;
+} */
+
+// Напишите программу, которая принимает на вход число N и выдает произведение чисел о 1 до N, 4 -> 24
+
+/*Console.Write("Введите А: ");
+Console.WriteLine(Summ(int.Parse(Console.ReadLine())));
+
+int Summ(int number)
+{
+    int sum=1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum *=i;
+    }
+    return sum;
+} */
+
+/* Напишите программу, которая выводит массив из 8 элеметов, заполненный нулями и единицами 
+в случайном порядке. [1, 0, 1, 1, 0, 0, 1, 0, 0] */
+
+/* int[] array = new int[8];
+
+
+  for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    Console.WriteLine(String.Join(" ", array)); */
---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+Console.Clear();
+
+/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"{num} -> {N(num)}");
+int sum = 0;
+
+int N (int num)
+{
+    int sum = 0;
+    while (num>0)
+ {
+    sum +=num%10;
+    num /=10;
+ }
+ return sum;
+}
