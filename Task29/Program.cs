3c46b6468a452e6d7971e11a426810c37008b3ce
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-11 21:23:23 +0300

    new folder task29

---
+++ Task29/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+Console.Clear();
+
+/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int[] mass = new int [8];
+string t = Console.ReadLine();;
+Console.WriteLine($"{String.Join(", ", t.ToArray())} -> ");
+
